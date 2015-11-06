8a8d644188d55dd19762cce9f3e3fdc652587724
Zachary Radtka <zachary.radtka@gmail.com>
2015-11-06 16:08:57 -0500

    Accumulo Added

---
+++ Accumulo/solutions/LocalEnvUtil/src/main/java/com/minerkasch/accumulo/examples/util/Constants.java
@@ -0,0 +1,14 @@
+package com.minerkasch.accumulo.examples.util;
+
+public class Constants {
+	// Accumulo Configuration
+	public static final String USER_NAME = "root";
+	public static final String USER_PASS = "password";
+	public static final String INSTANCE = "hdp-accumulo-instance";
+	public static final String ZOOKEEPERS = "localhost:2181";
+
+	// Table names
+	public static final String TWITTER_TABLE = "TwitterData";
+	public static final String TWEET_INDEX_TABLE = "TweetIndex";
+	public static final String TWEET_LOCATION_INDEX_TABLE = "TweetLocationIndex";
+}
