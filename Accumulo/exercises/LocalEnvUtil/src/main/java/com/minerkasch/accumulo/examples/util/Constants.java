41c3d2cc17ef7366506a6a10a5fa816e4a9a4ebf
Zachary Radtka <zachary.radtka@gmail.com>
2015-11-06 16:12:23 -0500

    Updated constants file

--- Accumulo/exercises/LocalEnvUtil/src/main/java/com/minerkasch/accumulo/examples/util/Constants.java
+++ Accumulo/exercises/LocalEnvUtil/src/main/java/com/minerkasch/accumulo/examples/util/Constants.java
@@ -3,8 +3,8 @@ package com.minerkasch.accumulo.examples.util;
 public class Constants {
 	// Accumulo Configuration
 	public static final String USER_NAME = "root";
-	public static final String USER_PASS = "secret";
-	public static final String INSTANCE = "localAccumulo";
+	public static final String USER_PASS = "password";
+	public static final String INSTANCE = "hdp-accumulo-instance";
 	public static final String ZOOKEEPERS = "localhost:2181";
 
 	// Table names
