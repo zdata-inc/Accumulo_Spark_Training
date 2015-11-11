c0c9dff0af17f35ae54b114d6d8b9bad69485bda
Zachary Radtka <zachary.radtka@gmail.com>
2015-11-11 23:50:40 +0100

    Graph Code Complete

--- Spark/GraphX/examples/graphx_lecture_code.scala
+++ Spark/GraphX/examples/graphx_lecture_code.scala
@@ -1,6 +1,7 @@
 import org.apache.spark.graphx._
 import org.apache.spark.rdd.RDD
 
+// Initialize the vertex array
 val vertexArray = Array(
   (1L, ("Alice", 28)),
   (2L, ("Bob", 27)),
@@ -9,6 +10,8 @@ val vertexArray = Array(
   (5L, ("Ed", 55)),
   (6L, ("Fran", 50))
   )
+
+// Initialize the edge array
 val edgeArray = Array(
   Edge(2L, 1L, 7),
   Edge(2L, 4L, 2),
@@ -20,25 +23,29 @@ val edgeArray = Array(
   Edge(5L, 6L, 3)
   )
 
+// Create an RDD for the vertices
 val vertexRDD: RDD[(Long, (String, Int))] = sc.parallelize(vertexArray)
+
+// Create an RDD for the edges
 val edgeRDD: RDD[Edge[Int]] = sc.parallelize(edgeArray)
 
+// Build the initial Graph
 val graph: Graph[(String, Int), Int] = Graph(vertexRDD, edgeRDD)
 
-// get names of the users that are at least 30 years old
-graph.vertices.filter { case (id, (name, age)) => age > 30 }.collect.foreach {
+// Get names of the users that are at least 30 years old
+graph.vertices.filter { case (id, (name, age)) => age > 30 }.collect().foreach {
   case (id, (name, age)) => println(s"$name is $age")
 }
 
-for ((id,(name,age)) <- graph.vertices.filter { case (id,(name,age)) => age > 30 }.collect) {
+// Another way to iterate over the users names
+for ((id,(name,age)) <- graph.vertices.filter { case (id,(name,age)) => age > 30 }.collect()) {
   println(s"$name is $age")
 }
 
-// get who likes who
+// Get who likes who
 for (triplet <- graph.triplets.collect) {
   println(s"${triplet.srcAttr._1} likes ${triplet.dstAttr._1}")
 }
-
 
 // Define a class to more clearly model the user property
 case class User(name: String, age: Int, inDeg: Int, outDeg: Int)
@@ -53,12 +60,12 @@ val userGraph = initialUserGraph.outerJoinVertices(initialUserGraph.inDegrees) {
   case (id, u, outDegOpt) => User(u.name, u.age, u.inDeg, outDegOpt.getOrElse(0))
 }
 
-// print the number of people who like each user: 
+// Print the number of people who like each user: 
 for ((id, property) <- userGraph.vertices.collect) {
   println(s"User $id is called ${property.name} and is liked by ${property.inDeg} people.")
 }
 
-// print the names of the users who are liked by the same number of people they like
+// Print the names of the users who are liked by the same number of people they like
 userGraph.vertices.filter {
   case (id, u) => u.inDeg == u.outDeg
 }.collect.foreach {
@@ -73,8 +80,7 @@ val oldestFollower: VertexRDD[(String, Int)] = userGraph.mapReduceTriplets[(Stri
   (a, b) => if (a._2 > b._2) a else b
   )
 
-
-// display oldest follower for each user
+// Display oldest follower for each user
 userGraph.vertices.leftJoin(oldestFollower) { (id, user, optOldestFollower) =>
   optOldestFollower match {
     case None => s"${user.name} does not have any followers."
@@ -82,6 +88,13 @@ userGraph.vertices.leftJoin(oldestFollower) { (id, user, optOldestFollower) =>
   }
 }.collect.foreach { case (id, str) => println(str) }
 
-// restrict graph to users 30 and over
+// Restrict graph to users 30 and over
 val olderGraph = userGraph.subgraph(vpred = (id, user) => user.age >= 30)
 
+// compute the connected components
+val cc = olderGraph.connectedComponents
+
+// display the component id of each user:
+olderGraph.vertices.leftJoin(cc.vertices) {
+  case (id, user, comp) => s"${user.name} is in component ${comp.get}"
+}.collect.foreach{ case (id, str) => println(str) }
